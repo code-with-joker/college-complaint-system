c99011c487b1ac57086a5caf0b742741b5ae4278
code-with-joker <bandadevil143@gmail.com>
2026-05-13 16:38:20 +0530

    switch to brevo smtp

--- backend/services/email.service.js
+++ backend/services/email.service.js
@@ -1,21 +1,14 @@
 const nodemailer = require("nodemailer");
 
-// 🔥 transporter (PRODUCTION SAFE)
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
+  host: "smtp-relay.brevo.com",
   port: 587,
   secure: false,
 
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  },
-
-  tls: {
-    rejectUnauthorized: false
-  },
-
-  family: 4 // ✅ FORCE IPv4 (IMPORTANT FOR RENDER)
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS
+  }
 });
 
 // 🔥 reusable send function
@@ -23,13 +16,13 @@ exports.sendEmail = async ({ to, subject, html }) => {
   try {
 
     await transporter.sendMail({
-      from: `"CampusResolve" <${process.env.EMAIL_USER}>`,
+      from: `"CampusResolve" <${process.env.SMTP_USER}>`,
       to,
       subject,
       html
     });
 
-    // console.log("📧 Email sent to:", to);
+    console.log("📧 Email sent to:", to);
 
     return true;
 
